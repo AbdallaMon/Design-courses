6215c150f379222981b39f0c1c5d178c2c60db8e
AbdallaMon <abdotlos60@gmail.com>
2025-07-12 12:38:04 +0300

    Init

---
+++ src/app/helpers/constants.js
@@ -0,0 +1,26 @@
+export const initialPageLimit = 10;
+export const totalLimitPages = [10, 20, 50, 100];
+export const simpleModalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  maxHeight: "90%",
+  overflow: "auto",
+  width: {
+    xs: "95%",
+    sm: "80%",
+    md: "60%",
+  },
+  maxWidth: {
+    md: "600px",
+  },
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  borderRadius: 2,
+  p: 4,
+};
+
+export const NotificationType = {
+  OTHER: "Other",
+};
